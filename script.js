f6982d617c887cab71533f32925c0bf3d29a49c0
Jay Nguyen <jwin@Jays-MBP.home>
2014-11-05 17:44:03 -0800

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-$(document).ready(function() {
-	$("#new_grid").click(function(){
-		var input = prompt("Please enter a number from 1-128 for grid size")
-		
-		if ()
-
-	});
+$(document).ready(function(){
+	gridFunction();
+	setColor("#white");
+});
+// button id="new_grid" prompts for grid size
+$("#new_grid").click(function(){
+	gridFunction();
+	setColor("#white");
 });
 
+function gridFunction() {
+	var input = prompt("Please enter a number from 1-128");
+	if (input >= 1 && input <=128) {
+		createGrid(input);
+	} else {
+		alert("Seriously, put a number 1-128. It'll make your life and mine easier.");
+	}
+}	
+
+function createGrid(max) {
+	for(var i=0; i < max; i++) {
+		var row=document.getElementbyId("gridTable").insertRow(i);
+
+		for(var j=0; j < max; j++) {
+			row.insertCell(j);
+		}
+	}
+}
